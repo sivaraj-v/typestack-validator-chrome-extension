bdc199e4ab5102da7291a5ad1a14ef2a1b97bc33
sivaraj-v <zural143@gmail.com>
2023-09-11 18:33:10 +0530

    Updated config with links

--- typestack-validator-live/contentscript.js
+++ typestack-validator-live/contentscript.js
@@ -5,78 +5,126 @@
  * Email : zural143@gmail.com
  */
 
-const DOM_Elements = [".tr-Typestack--h1", ".tr-Typestack--h2", ".tr-Typestack--h3", ".tr-Typestack--h4", ".tr-Typestack--h5", ".tr-Typestack--subline1", ".tr-Typestack--subline2", ".tr-Typestack--p1", ".tr-Typestack--p1Bold", ".tr-Typestack--p2", ".tr-Typestack--p2Bold", ".tr-Typestack--p3", ".tr-Typestack--p3Bold"];
-const DOM_Elements_As_Class = ["tr-Typestack--h1", "tr-Typestack--h2", "tr-Typestack--h3", "tr-Typestack--h4", "tr-Typestack--h5", "tr-Typestack--subline1", "tr-Typestack--subline2", "tr-Typestack--p1", "tr-Typestack--p1Bold", "tr-Typestack--p2", "tr-Typestack--p2Bold", "tr-Typestack--p3", "tr-Typestack--p3Bold"];
 const typestack = {
     "tr-Typestack--h1": {
         fontSize: "44px",
         fontWeight: "500",
-        lineHeight: "56px"
+        lineHeight: "56px",
+        textDecoration : "none solid rgb(26, 26, 26)"
     },
     "tr-Typestack--h2": {
         fontSize: "32px",
         fontWeight: "500",
-        lineHeight: "40px"
+        lineHeight: "40px",
+        textDecoration : "none solid rgb(26, 26, 26)"
     },
     "tr-Typestack--h3": {
         fontSize: "28px",
         fontWeight: "600",
-        lineHeight: "36px"
+        lineHeight: "36px",
+        textDecoration : "none solid rgb(26, 26, 26)"
     },
     "tr-Typestack--h4": {
         fontSize: "24px",
         fontWeight: "600",
-        lineHeight: "32px"
+        lineHeight: "32px",
+        textDecoration : "none solid rgb(26, 26, 26)"
     },
     "tr-Typestack--h5": {
         fontSize: "20px",
         fontWeight: "600",
-        lineHeight: "28px"
+        lineHeight: "28px",
+        textDecoration : "none solid rgb(26, 26, 26)"
     },
     "tr-Typestack--subline1": {
         fontSize: "12px",
         fontWeight: "600",
-        lineHeight: "20px"
+        lineHeight: "20px",
+        textDecoration : "none solid rgb(77, 77, 77)"
     },
     "tr-Typestack--subline2": {
         fontSize: "20px",
         fontWeight: "400",
-        lineHeight: "28px"
+        lineHeight: "28px",
+        textDecoration : "none solid rgb(77, 77, 77)"
     },
     "tr-Typestack--p1": {
         fontSize: "16px",
         fontWeight: "400",
-        lineHeight: "24px"
+        lineHeight: "24px",
+        textDecoration : "none solid rgb(26, 26, 26)"
+    },
+    "tr-Typestack--p1Link": {
+        fontSize: "16px",
+        fontWeight: "400",
+        lineHeight: "24px",
+        textDecoration : "underline solid rgb(77, 77, 77)"
     },
     "tr-Typestack--p1Bold": {
         fontSize: "16px",
         fontWeight: "700",
-        lineHeight: "24px"
+        lineHeight: "24px",
+        textDecoration : "none solid rgb(77, 77, 77)"
     },
     "tr-Typestack--p2": {
         fontSize: "14px",
         fontWeight: "400",
-        lineHeight: "20px"
+        lineHeight: "20px",
+        textDecoration : "none solid rgb(77, 77, 77)"
+    },
+    "tr-Typestack--p2Link": {
+        fontSize: "14px",
+        fontWeight: "400",
+        lineHeight: "20px",
+        textDecoration : "underline solid rgb(77, 77, 77)"
     },
     "tr-Typestack--p2Bold": {
         fontSize: "14px",
         fontWeight: "700",
-        lineHeight: "20px"
+        lineHeight: "20px",
+        textDecoration : "none solid rgb(77, 77, 77)"
     },
     "tr-Typestack--p3": {
         fontSize: "12px",
         fontWeight: "400",
-        lineHeight: "16px"
+        lineHeight: "16px",
+        textDecoration : "none solid rgb(77, 77, 77)"
+    },
+    "tr-Typestack--p3Link": {
+        fontSize: "12px",
+        fontWeight: "400",
+        lineHeight: "16px",
+        textDecoration : "underline solid rgb(77, 77, 77)"
     },
     "tr-Typestack--p3Bold": {
         fontSize: "12px",
         fontWeight: "700",
-        lineHeight: "16px"
+        lineHeight: "16px",
+        textDecoration : "none solid rgb(77, 77, 77)"
     }
 };
-const display={"tr-Typestack--h1":"H1","tr-Typestack--h2":"H2","tr-Typestack--h3":"H3","tr-Typestack--h4":"H4","tr-Typestack--h5":"H5","tr-Typestack--subline1":"S1","tr-Typestack--subline2":"S2","tr-Typestack--p1":"P1","tr-Typestack--p1Bold":"P1 Bold","tr-Typestack--p2":"P2","tr-Typestack--p2Bold":"P2 Bold","tr-Typestack--p3":"P1","tr-Typestack--p3Bold":"P3 Bold"};
+const display = {
+    "tr-Typestack--h1": "H1",
+    "tr-Typestack--h2": "H2",
+    "tr-Typestack--h3": "H3",
+    "tr-Typestack--h4": "H4",
+    "tr-Typestack--h5": "H5",
+    "tr-Typestack--subline1": "S1",
+    "tr-Typestack--subline2": "S2",
+    "tr-Typestack--p1": "P1",
+    "tr-Typestack--p1Link": "P1 Link",
+    "tr-Typestack--p1Bold": "P1 Bold",
+    "tr-Typestack--p2": "P2",
+    "tr-Typestack--p2Link": "P2 Link",
+    "tr-Typestack--p2Bold": "P2 Bold",
+    "tr-Typestack--p3": "P3",
+    "tr-Typestack--p3Link": "P3 Link",
+    "tr-Typestack--p3Bold": "P3 Bold"
+  };
 
-const DOM_Elements_Joined = document.querySelectorAll(`${DOM_Elements.join(",")}`);
+const typestackKeys = Object.keys(typestack);
+const classNamesWithPeriod = typestackKeys.map(className => `.${className}`);
+const DOM_Elements_Joined = document.querySelectorAll(`${classNamesWithPeriod.join(",")}`);
 
 function compareObjects(obj1, obj2) {
 	const keys1 = Object.keys(obj1);
@@ -102,7 +150,8 @@ for (const element of DOM_Elements_Joined) {
 	const filteredProperties = [
 		'fontSize',
 		'fontWeight',
-		'lineHeight'
+		'lineHeight',
+        "textDecoration"
 	];
 
 	const filteredStyles = {};
@@ -114,8 +163,8 @@ for (const element of DOM_Elements_Joined) {
 	}
 	
 	const classListArray = Array.from(element.classList);
-  	const filteredClassNames = DOM_Elements_As_Class.filter(className => classListArray.includes(className));		
-	const areEqual = compareObjects(filteredStyles, typestack[filteredClassNames.toString()]);
+    const filteredClassNames = typestackKeys.filter(className => classListArray.includes(className));
+    const areEqual = compareObjects(filteredStyles, typestack[filteredClassNames.toString()]);
 
 	if (areEqual) {
 		const spanElement = document.createElement('span');
